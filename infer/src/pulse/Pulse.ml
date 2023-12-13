8f6e7d9154579d556abc202e09a76707caf0c0f9
liwenhua1 <liwenhua@comp.nus.edu.sg>
2023-12-13 16:52:45 +0800

    m

--- infer/src/pulse/Pulse.ml
+++ infer/src/pulse/Pulse.ml
@@ -1278,14 +1278,15 @@ module PulseTransferFunctions = struct
           |None ->
             let static_ty = AbductiveDomain.AddressAttributes.get_static_type call_obj astate in 
             (match static_ty with
-            |Some sty -> let possible_subclass = Tenv.find_limited_sub sty tenv in 
+            |Some sty -> let possible_subclass = sty::(Tenv.find_limited_sub sty tenv) in 
+           
             let astate_n = check_modified_before_dtor actuals call_exp astate astate_n in
             let constrains = Formula.get_all_instance_constrains call_obj astate.path_condition in 
             let yes_instance = Formula.type_list_conversion (fst constrains) in 
             let not_instance = Formula.type_list_conversion (snd constrains) in 
             let possible_subclass_sat = List.fold possible_subclass ~init:[] ~f:(fun acc x -> if Formula.check_dynamic_type_sat x (yes_instance,not_instance) tenv then x::acc else acc ) in
 
-
+            
             let astates_all, astates_before_all =  List.fold possible_subclass_sat ~init:([],[]) ~f:( fun (ast,astb) cls_name -> 
             
             let call_exp = 
@@ -1663,13 +1664,13 @@ let analyze specialization
     let ((exit_summaries_opt:DisjunctiveAnalyzer.TransferFunctions.Domain.t option), exn_sink_summaries_opt) =
       DisjunctiveAnalyzer.compute_post_including_exceptional analysis_data ~initial proc_desc
     in
-    (* print_endline "process analysis";
+    print_endline "process analysis";
     Procname.pp_name_only F.std_formatter proc_name;
     let res = match exit_summaries_opt with 
     | None  -> ()
     | Some a -> DisjunctiveAnalyzer.TransferFunctions.Domain.pp F.std_formatter a in
     res;
-    print_endline "process analysis end"; *)
+    print_endline "process analysis end";
     let process_postconditions node posts_opt ~convert_normal_to_exceptional =
       match posts_opt with
       | Some (posts, non_disj_astate) ->
